08e9399a76519b899ecad893af5b38c568e701de
Rafael Ribeiro <rafael.ribeirowt@gmail.com>
2022-12-28 08:40:42 -0300

    Api calculadora

---
+++ calculadoraIMC.js
@@ -0,0 +1,46 @@
+function efetuarCalculoImc(peso, altura){
+    let imc = peso / (altura * altura)
+    return imc;
+
+}
+
+
+function retornaStatusIMC(imc)
+{
+    let status;
+
+    if(imc < 18.5)
+    {
+        status = 'Abaixo do peso';
+    }
+    else if(imc >= 18.5 && imc < 24.9)
+    {
+        status = 'Peso Normal';
+    }
+    else if(imc >= 24.9 && imc < 30)
+    {
+        status = 'Acima do peso';
+    }
+    else
+    {
+        status = 'Obeso';
+    }
+
+    return status;
+}
+
+function validaParametro(parametro)
+{
+    if(isNaN(parametro))
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+exports.efetuarCalculoImc = efetuarCalculoImc;
+exports.retornaStatusIMC = retornaStatusIMC;
+exports.validaParametro = validaParametro;
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+
+const calculadoraIMC = require('./calculadoraIMC');
+
+
+app.get('/', (req, res) => {
+    let peso = req.query.peso;
+    let altura =  req.query.altura;
+
+    if(calculadoraIMC.validaParametro(req.query.peso) && calculadoraIMC.validaParametro(req.query.altura)){
+    let imc = calculadoraIMC.efetuarCalculoImc(peso , altura);
+    let status = calculadoraIMC.retornaStatusIMC(imc);
+
+    let json = {imc: imc, status: status};
+
+    res.json(json);
+}else{
+    res.status(400).json({'Erro':'Peso ou altura inválidos.'});
+}
+});
+
+app.listen(8080, () => {
+    let data = new Date();
+    console.log('Servidor node iniciado em: ' + data);
+});
